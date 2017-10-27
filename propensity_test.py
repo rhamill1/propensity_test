66051395dfe59acb11cce1192f91ac960a8dd19c
Hamill <DKS0448705@dcsg.com>
2017-10-27 11:08:39 -0400

    adds sklearn to project

--- propensity_test.py
+++ propensity_test.py
@@ -1,7 +1,17 @@
-from causalinference import CausalModel
-from causalinference.utils import random_data
+# from causalinference import CausalModel
+# from causalinference.utils import random_data
+# 
+# Y, D, X = random_data()
+# causal = CausalModel(Y, D, X)
+# 
+# print(causal)
+# print(Y)
 
-Y, D, X = random_data()
-causal = CausalModel(Y, D, X)
 
-print('it ran')
+from sklearn import datasets
+
+iris = datasets.load_iris()
+digits = datasets.load_digits()
+
+#print(iris)
+print(digits.data)
